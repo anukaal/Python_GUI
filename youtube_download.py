d50634a465d1872b9270d3a8dfdc95edf0e28eb2
anukaal <mailanu98@gmail.com>
2020-07-14 16:37:24 +0530

    get link

--- youtube_download.py
+++ youtube_download.py
@@ -7,3 +7,28 @@ from pytube import Youtube
 # initializing tkinter
 root = Tk()
 
+# setting the geometry of the GUI
+
+root.geometry("400*350")
+
+# setting the title of the GUI
+
+root.title("Youtube video downloader application")
+
+# defining download function
+
+def download():
+    # using try and except to excute program without errors
+
+    try:
+        myVar.set("Downloading....")
+        root.update()
+        YouTube(link.get()).stremas.first().download()
+        link.set("Video download successfully")
+    
+    except Exception as e:
+        myVar.set("Mistake")
+        root.update()
+        link.set("Enter correct link")
+
+
