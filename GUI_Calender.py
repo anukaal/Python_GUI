e899bcb4b1876ac6d92015ec810506d234226ff0
anukaal <mailanu98@gmail.com>
2020-07-17 14:34:01 +0530

    added Year

--- GUI_Calender.py
+++ GUI_Calender.py
@@ -4,3 +4,12 @@ from tkinter import *
 
 # importing calendar module
 import calendar
+
+# initializing tkinter
+root = Tk()
+
+# setting title of our Gui
+root.title("My Own Gui Calendar")
+
+# year for which we want the calendar to be shown on our Gui
+year = 2020
