839c3e745e83de2636a8072d41f98620280f8afd
anukaal <mailanu98@gmail.com>
2020-07-21 21:17:53 +0530

    importing and setting

---
+++ covid_GUI.py
@@ -0,0 +1,11 @@
+# importing tkinter
+from tkinter import *
+
+# initializing tkinter
+root = Tk()
+
+# setting geometry
+root.geometry("350x350")
+
+# setting title
+root.title("Get Covid-19 Data Country Wise")
