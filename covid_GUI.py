0f81bfca1292209301eac842504b688602015adc
anukaal <mailanu98@gmail.com>
2020-07-24 13:00:49 +0530

    add function

--- covid_GUI.py
+++ covid_GUI.py
@@ -1,3 +1,4 @@
+
 # importing tkinter
 from tkinter import *
 
@@ -9,3 +10,21 @@ root.geometry("350x350")
 
 # setting title
 root.title("Get Covid-19 Data Country Wise")
+
+# function which will get covid data and will show it
+def showdata():
+    # importing matplotlib which will be used to show data graphically
+    from matplotlib import pyplot as plt
+    # to scale the data we are importing patches
+    import matplotlib.patches as mpatches
+    # importing covid library
+    from covid import Covid
+    # initializing covid library
+    covid = Covid()
+    # declaring empty lists to store different data sets
+    cases = []
+    confirmed = []
+    active = []
+    deaths = []
+    recovered = []
+    # using try and except to run program without errors
