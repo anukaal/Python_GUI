8958e9d8a6bc025713419b918e260566f4ab2e6b
anukaal <mailanu98@gmail.com>
2020-07-25 18:34:49 +0530

    add country data

--- covid_GUI.py
+++ covid_GUI.py
@@ -28,3 +28,28 @@ def showdata():
     deaths = []
     recovered = []
     # using try and except to run program without errors
+
+    try:
+         # updating root
+        root.update()
+        # getting countries names entered by the user
+        countries = data.get()
+        # removing white spaces from the start and end of the string
+        country_names = countries.strip()
+        # replacing white spaces with commas inside the string
+        country_names = country_names.replace(" ", ",")
+        # splitting the string to store names of countries
+        # as a list 
+        country_names = country_names.split(",")
+        # for loop to get all countries data
+        for x in country_names:
+            # appending countries data one-by-one in cases list 
+            # here, the data will be stored as a dictionary
+            # for one country i.e. for each country
+            # there will be one dictionary in the list
+            # which will contain the whole information
+            # of that country
+            cases.append(covid.get_status_by_country_name(x))
+            # updating the root
+            root.update()
+        # for loop to get one country data stored as dict in list cases
